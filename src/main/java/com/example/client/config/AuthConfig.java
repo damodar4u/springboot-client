fda44d1992ad54257e87488ee289d8edd6c9920a
Damodar <damodar.neelapu@yahoo.com>
2024-12-03 14:38:14 -0500

    Create AuthConfig.java

---
+++ src/main/java/com/example/client/config/AuthConfig.java
@@ -0,0 +1,25 @@
+package com.example.client.config;
+
+public class AuthConfig {
+    private final String clientId;
+    private final String clientSecret;
+    private final String redirectUri;
+
+    public AuthConfig(String clientId, String clientSecret, String redirectUri) {
+        this.clientId = clientId;
+        this.clientSecret = clientSecret;
+        this.redirectUri = redirectUri;
+    }
+
+    public String getClientId() {
+        return clientId;
+    }
+
+    public String getClientSecret() {
+        return clientSecret;
+    }
+
+    public String getRedirectUri() {
+        return redirectUri;
+    }
+}
