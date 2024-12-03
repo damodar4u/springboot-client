f8c54ba69913792ea322e0dd989952b04ddba5a7
Damodar <damodar.neelapu@yahoo.com>
2024-12-03 15:04:09 -0500

    Update AuthController.java

--- src/main/java/com/example/client/controller/AuthController.java
+++ src/main/java/com/example/client/controller/AuthController.java
@@ -1,13 +1,15 @@
 package com.example.client.controller;
 
 import com.example.client.config.AuthConfig;
-import com.example.authlib.utils.TokenUtils;
+import com.example.authlib.servlets.LoginServlet;
+import com.example.authlib.servlets.AuthCallbackServlet;
 import com.auth0.jwt.interfaces.Claim;
-import com.microsoft.aad.msal4j.IAuthenticationResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.Map;
 
 @RestController
@@ -20,22 +22,14 @@ public class AuthController {
     );
 
     @GetMapping("/login")
-    public String login() {
-        // Generate login URL
-        String loginUrl = "https://login.microsoftonline.com/{tenant}/oauth2/v2.0/authorize" +
-                "?client_id=" + authConfig.getClientId() +
-                "&response_type=code" +
-                "&redirect_uri=" + authConfig.getRedirectUri() +
-                "&scope=openid profile email";
-        return "Login URL: " + loginUrl;
+    public void login(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        // Delegate login functionality to the JAR
+        new LoginServlet(authConfig).doGet(request, response);
     }
 
     @GetMapping("/callback")
-    public Map<String, Claim> callback(@RequestParam("code") String code) throws Exception {
-        // Exchange auth code for token
-        IAuthenticationResult result = TokenUtils.acquireTokenWithAuthCode(code, authConfig);
-
-        // Extract and return claims
-        return TokenUtils.extractClaims(result.idToken());
+    public Map<String, Claim> callback(@RequestParam("code") String code, HttpServletRequest request, HttpServletResponse response) throws Exception {
+        // Delegate callback processing to the JAR
+        return new AuthCallbackServlet(authConfig).handleCallback(code, request, response);
     }
 }
